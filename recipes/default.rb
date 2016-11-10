cad6b4c8bf932c45b9fb641f246a93e9863fb4f3
Alexey Terekhov <alexey.terekhov.ingate@gmail.com>
2016-11-10 14:37:24 +0300

    add different service providers for different versions of ubuntu

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,54 +1,69 @@
-service "ssh"
+# https://github.com/chef-cookbooks/openssh/pull/62
 
-ssh_config "Port" do
-  string "Port #{node[:ssh][:port]}"
+service_provider = nil
+
+if node['platform'] == 'ubuntu'
+  if Chef::VersionConstraint.new('>= 15.04').include?(node['platform_version'])
+    service_provider = Chef::Provider::Service::Systemd
+  elsif Chef::VersionConstraint.new('>= 12.04').include?(node['platform_version'])
+    service_provider = Chef::Provider::Service::Upstart
+  end
 end
 
-ssh_config "PermitRootLogin" do
-  string "PermitRootLogin #{node[:ssh][:permit_root_login]}"
+service 'ssh' do
+  provider service_provider
+  action [:enable, :start]
 end
 
-ssh_config "PasswordAuthentication" do
-  string "PasswordAuthentication #{node[:ssh][:password_authentication]}"
+ssh_config 'Port' do
+  string "Port #{node['ssh'][:port]}"
 end
 
-ssh_config "PermitEmptyPasswords" do
-  string "PermitEmptyPasswords #{node[:ssh][:permit_empty_passwords]}"
+ssh_config 'PermitRootLogin' do
+  string "PermitRootLogin #{node['ssh']['permit_root_login']}"
 end
 
-ssh_config "ChallengeResponseAuthentication" do
-  string "ChallengeResponseAuthentication #{node[:ssh][:challenge_response_authentication]}"
+ssh_config 'PasswordAuthentication' do
+  string "PasswordAuthentication #{node['ssh']['password_authentication']}"
 end
 
-ssh_config "UsePAM" do
-  string "UsePAM #{node[:ssh][:use_pam]}"
+ssh_config 'PermitEmptyPasswords' do
+  string "PermitEmptyPasswords #{node['ssh']['permit_empty_passwords']}"
 end
 
-ssh_config "UseDNS" do
-  string "UseDNS #{node[:ssh][:use_dns]}"
+ssh_config 'ChallengeResponseAuthentication' do
+  string "ChallengeResponseAuthentication #{node['ssh']['challenge_response_authentication']}"
 end
 
-ssh_config "AllowGroups" do
-  string "AllowGroups #{node[:ssh][:allowed_groups].join(' ')}"
-  only_if { node[:ssh][:allowed_groups].any? }
+ssh_config 'UsePAM' do
+  string "UsePAM #{node['ssh']['use_pam']}"
 end
 
-ssh_config "AllowUsers" do
-  string "AllowUsers #{node[:ssh][:allowed_users].join(' ')}"
-  only_if { node[:ssh][:allowed_users].any? }
+ssh_config 'UseDNS' do
+  string "UseDNS #{node['ssh']['use_dns']}"
 end
 
-ssh_config "DenyGroups" do
-  string "DenyGroups #{node[:ssh][:denied_groups].join(' ')}"
-  only_if { node[:ssh][:denied_groups].any? }
+ssh_config 'AllowGroups' do
+  string "AllowGroups #{node['ssh']['allowed_groups'].join(' ')}"
+  only_if { node['ssh']['allowed_groups'].any? }
 end
 
-ssh_config "DenyUsers" do
-  string "DenyUsers #{node[:ssh][:denied_users].join(' ')}"
-  only_if { node[:ssh][:denied_users].any? }
+ssh_config 'AllowUsers' do
+  string "AllowUsers #{node['ssh']['allowed_users'].join(' ')}"
+  only_if { node['ssh']['allowed_users'].any? }
 end
 
-#ssh_authorized_keys "root" do
-#  home "/root"
-#  ssh_keys node[:ssh][:root_authorized_keys]
+ssh_config 'DenyGroups' do
+  string "DenyGroups #{node['ssh']['denied_groups'].join(' ')}"
+  only_if { node['ssh']['denied_groups'].any? }
+end
+
+ssh_config 'DenyUsers' do
+  string "DenyUsers #{node['ssh']['denied_users'].join(' ')}"
+  only_if { node['ssh']['denied_users'].any? }
+end
+
+#ssh_authorized_keys 'root' do
+#  home '/root'
+#  ssh_keys node['ssh']['root_authorized_keys']
 #end
--- resources/ssh_config.rb
+++ resources/ssh_config.rb
@@ -14,7 +14,7 @@ action :add do
       fi
     }
     not_if %{ egrep -c "^#{new_resource.string}$" /etc/ssh/sshd_config -q }
-    notifies :restart, resources(service: "ssh"), :delayed
+    notifies :restart, 'service[ssh]', :delayed
   end
 end
 
@@ -25,7 +25,7 @@ action :add_multiline do
       echo -en "#{new_resource.string}\n" >> /etc/ssh/sshd_config
     }
     not_if %{ [[ ! $(cat /etc/ssh/sshd_config) =~ "#{new_resource.match}" ]] }
-    notifies :restart, resources(service: "ssh"), :delayed
+    notifies :restart, 'service[ssh]', :delayed
   end
 end
 
@@ -36,6 +36,6 @@ action :remove do
       sed -i '/#{new_resource.match}.*/ d' /etc/ssh/sshd_config
     }
     only_if %{ egrep -c "^#{new_resource.string}$" /etc/ssh/sshd_config -q }
-    notifies :restart, resources(service: "ssh"), :delayed
+    notifies :restart, 'service[ssh]', :delayed
   end
 end
